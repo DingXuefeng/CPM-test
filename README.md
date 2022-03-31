d1bd4a81c2833bd11b2eed83a3ef689843244c7d
Xuefeng, Ding <xuefeng.ding.physics@gmail.com>
2022-03-31 13:15:37 -0400

    add initial files
    add initial files
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    bug fix
    
    add comments

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # CPM-test
 testing CPM-CMAKE (https://github.com/cpm-cmake/CPM.cmake)
+
+## Add CPM
+- download, then add with git
+```shell
+mkdir -p cmake
+wget -O cmake/CPM.cmake https://github.com/cpm-cmake/CPM.cmake/releases/latest/download/get_cpm.cmake
+```
---
+++ tests.cc
@@ -0,0 +1,16 @@
+/*****************************************************************************/
+// Author: Xuefeng Ding <xuefengd@princeton.edu>
+// Institute: Department of Physics, Princeton University
+// Date: 3/31/22
+// Version: v1.0
+// Description: 
+//
+// All rights reserved. 2022 copyrighted.
+// Copyright (c) 2022
+/*****************************************************************************/
+
+#include "gtest/gtest.h"
+
+TEST(myTEST,1) {
+  ASSERT_EQ(1,1);
+}
